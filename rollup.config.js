dc30b89ca3c00a392bac7793c33ad62b4ebc1714
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2021-06-23 14:05:20 +0200

    Hopefully fixed typings derp

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import ts from 'rollup-plugin-ts';
 
 const pkg = require("./package.json")
 
@@ -18,8 +18,8 @@ export default [{
     }],
     plugins: [
         peerDepsExternal(),
+        ts(),
         resolve(),
-        commonjs(),
-        typescript()
+        commonjs()
     ]
-},]
+}]
