09753dedb48a105288a0b48dab36cd62797e1f2a
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-10-05 19:18:52 -0400

    General Refactoring

--- src/getName.ts
+++ src/getName.ts
@@ -1,13 +1,38 @@
-export function getName(props: any): string {
-    if ('displayName' in props && typeof props.displayName == 'string')
-        return props.displayName
-    if (
-        typeof props.for == 'function'
-        || typeof props.for == 'object'
-    ) {
-        const name = props.for.displayName ?? props.for.name
-            ?? props.for.default?.displayName ?? props.for.default?.name
-        if (typeof name == 'string') return name
-    }
-    return 'component'
+
+/**
+ *  @brief Determine the name of a props.
+ */
+
+export function getName ( props : any ) : string {
+    return displayNameOf(props)
+        ?? forNameOf(props)
+        ?? 'component'
+}
+
+
+function displayNameOf ( props : any ) : string | undefined {
+    
+    const name = props.displayName;
+    
+    if(typeof name == 'string')
+        return name;
+}
+
+function forNameOf ( props : any ) : string | undefined {
+    
+    const 
+        value = props.for ,
+        type = typeof value ;
+    
+    if( type != 'function' && type != 'object' )
+        return;
+        
+    const name = 
+        value.displayName ?? 
+        value.name ?? 
+        value.default?.displayName ??
+        value.default?.name;
+              
+    if(typeof name == 'string')
+        return name;
 }
